6058a940be56c534405904c65cb07952fe50dc4b
Karl Heitmann <khf1988@gmail.com>
2021-04-07 11:22:42 -0400

    genera los cards

--- assets/js/generar.js
+++ assets/js/generar.js
@@ -22,33 +22,53 @@ let galeria = document.getElementById('los-talentos');
 talentos.forEach(talento => {
   console.log("Talento", talento)
 
+  // Contenedor col
   let div_col = document.createElement('div');
   div_col.setAttribute('class', 'col-lg-3 pb-4');
 
+  // Contenedor card, este contiene un <img> y un <div class="card-body">
   let div_card = document.createElement('div');
+  div_col.appendChild(div_card)
   div_card.setAttribute('class', 'card text-center font-weight-light border-white');
-
-  let p_descripcion = document.createElement('p');
-  p_descripcion.innerText = talento.descripcion;
-
-  let a_link = document.createElement('a');
-  a_link.setAttribute("href", talento.link);
-  // a_link.setAttribute("href", repo.html_url);
-
-  a_link.innerText = talento.nombre;
-  a_link.setAttribute('target', '_blank');
-  a_link.setAttribute('class', 'galeria-proyecto__link');
 
   let img_perfil = document.createElement('img');
   img_perfil.setAttribute('src', talento.imagen);
   // img_perfil.setAttribute('src', 'assets/img/chef_armando.jpg')
   img_perfil.setAttribute('class', 'shadow-lg rounded');
-
-  div_card.appendChild(a_link);
-  div_card.appendChild(p_descripcion);
   div_card.appendChild(img_perfil);
 
-  div_col.appendChild(div_card);
+  let div_card_body = document.createElement('div');
+  div_card_body.setAttribute('class', 'card-body bg-white shadow p-3 mb-5 rounded text-dark')
+  div_card.appendChild(div_card_body);
+
+  // Contenedor info: nombre y profesion
+  let div_card_info = document.createElement('div');
+  div_card_body.appendChild(div_card_info);
+  div_card_info.setAttribute('class', 'info text-left');
+
+  let h4_name = document.createElement('h4');
+  div_card_info.appendChild(h4_name);
+  h4_name.setAttribute('class', 'name');
+  h4_name.innerText = talento.nombre;
+
+  let p_descripcion = document.createElement('p');
+  div_card_info.appendChild(p_descripcion);
+  p_descripcion.setAttribute('class', 'profesion');
+  p_descripcion.innerText = talento.descripcion;
+
+  // let a_link = document.createElement('a');
+  // a_link.setAttribute("href", talento.link);
+  // // a_link.setAttribute("href", repo.html_url);
+
+  // a_link.innerText = talento.nombre;
+  // a_link.setAttribute('target', '_blank');
+  // a_link.setAttribute('class', 'galeria-proyecto__link');
+
+
+  // div_card.appendChild(a_link);
+  // div_card.appendChild(p_descripcion);
+
+  // div_col.appendChild(div_card);
 
   galeria.appendChild(div_col);
 

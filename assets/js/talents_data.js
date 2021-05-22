3dad79716de09731e605ac7175436f238e96e9ea
Shirley Alvarado <shirley@codecrc.com>
2021-05-21 19:25:25 -0600

    Modified Shirley Card

--- assets/js/talents_data.js
+++ assets/js/talents_data.js
@@ -1,266 +1,264 @@
-const talents = [
-  {
-    nombre: 'Rogelio Chanto',
-    profesion: 'Full Stack / Asesor Finaciero',
-    imagen: 'assets/img/chef_armando.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Jaqueline Picado',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/jaqueline.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Erick Rojas',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/erick-rojas.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Shirley Alvarado',
-    profesion: 'Full Stack / Designer',
-    imagen: 'assets/img/shirley.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Henry Artavia',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/henry.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Oscar Vega',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/oscar.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Elian Rojas',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/foto.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Alonso Viales A.',
-    profesion: 'Salud Ocupacional',
-    imagen: 'assets/img/alonso.jpg',
-    home_page: 'https://aviales.github.io/Curriculum-Vitae/.',
-    redes_sociales: [
-      {link: 'https://github.com/aviales', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Mauricio Badilla',
-    profesion: 'Full Stack, Agente',
-    imagen: 'assets/img/foto.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Azalea Acuña',
-    profesion: 'Senior Designer',
-    imagen: 'assets/img/azalea.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Cynthia Salazar',
-    profesion: 'Full Stack/Finance',
-    imagen: 'assets/img/cynthia1.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Erick Ramos',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/erick_ramos.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Fabián Conejo',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/fabian.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Gabriela Solís',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/gaby.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Jordan Sandoval',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/jordan.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Joseph Murillo',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/joseph.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Lidya Arce',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/lydia.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Marcela Brenes',
-    profesion: 'Full Stack / Criminóloga',
-    imagen: 'assets/img/marcela.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Alejandra Serrano',
-    profesion: 'Full Stack / Relacionista Pública',
-    imagen: 'assets/img/alejandra.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Pablo',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/foto.png',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Randall Quesada',
-    profesion: 'Full Stack / Músico',
-    imagen: 'assets/img/randall.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Roxana Chinchilla',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/roxi.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Steyler Ayala',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/styler.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
-  {
-    nombre: 'Yirlania Torres',
-    profesion: 'Full Stack',
-    imagen: 'assets/img/yirlania.jpg',
-    home_page: '#',
-    redes_sociales: [
-      {link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1'},
-      {link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1'},
-    ]
-  },
+const talents = [{
+        nombre: 'Rogelio Chanto',
+        profesion: 'Full Stack / Asesor Finaciero',
+        imagen: 'assets/img/chef_armando.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Jaqueline Picado',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/jaqueline.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Erick Rojas',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/erick-rojas.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Shirley Alvarado',
+        profesion: 'Full Stack / Web Designer',
+        imagen: 'assets/img/shirley.jpg',
+        home_page: 'https://github.com/Chesza-Chesza',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: 'https://www.linkedin.com/in/shirley-alvarado-soto', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Henry Artavia',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/henry.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Oscar Vega',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/oscar.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Elian Rojas',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/foto.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Alonso Viales A.',
+        profesion: 'Salud Ocupacional',
+        imagen: 'assets/img/alonso.jpg',
+        home_page: 'https://aviales.github.io/Curriculum-Vitae/.',
+        redes_sociales: [
+            { link: 'https://github.com/aviales', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Mauricio Badilla',
+        profesion: 'Full Stack, Agente',
+        imagen: 'assets/img/foto.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Azalea Acuña',
+        profesion: 'Senior Designer',
+        imagen: 'assets/img/azalea.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Cynthia Salazar',
+        profesion: 'Full Stack/Finance',
+        imagen: 'assets/img/cynthia1.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Erick Ramos',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/erick_ramos.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Fabián Conejo',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/fabian.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Gabriela Solís',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/gaby.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Jordan Sandoval',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/jordan.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Joseph Murillo',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/joseph.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Lidya Arce',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/lydia.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Marcela Brenes',
+        profesion: 'Full Stack / Criminóloga',
+        imagen: 'assets/img/marcela.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Alejandra Serrano',
+        profesion: 'Full Stack / Relacionista Pública',
+        imagen: 'assets/img/alejandra.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Pablo',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/foto.png',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Randall Quesada',
+        profesion: 'Full Stack / Músico',
+        imagen: 'assets/img/randall.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Roxana Chinchilla',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/roxi.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Steyler Ayala',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/styler.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
+    {
+        nombre: 'Yirlania Torres',
+        profesion: 'Full Stack',
+        imagen: 'assets/img/yirlania.jpg',
+        home_page: '#',
+        redes_sociales: [
+            { link: '#', class_iconos: 'fab fa-github-square fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-linkedin fa-2x pl-1' },
+            { link: '#', class_iconos: 'fab fa-facebook-square fa-2x pl-1' },
+        ]
+    },
 ]
